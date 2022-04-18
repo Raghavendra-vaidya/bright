b9b8d08f6680c3397bddb61ebb271115762f398a
Raghavendra vaidya <rkvaidya58@gmail.com>
2022-04-18 23:26:18 +0530

    chore(suite): add utils :zap:

--- src/main/java/com/bright/configuration/CreateWebDriver.java
+++ src/main/java/com/bright/configuration/CreateWebDriver.java
@@ -1,4 +1,28 @@
 package com.bright.configuration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class CreateWebDriver {
+    static  WebDriver driver;
+
+
+    static WebDriver instance(String browser){
+        if (browser.equalsIgnoreCase("chrome")){
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--start-maximized");
+            driver = new ChromeDriver(chromeOptions);
+
+        }
+        else if (browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+        }
+
+        return driver;
+    }
+
+
 }
--- src/main/java/com/bright/projectutilities/GetData.java
+++ src/main/java/com/bright/projectutilities/GetData.java
@@ -1,4 +1,31 @@
 package com.bright.projectutilities;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class GetData {
+
+    static String fromProperties(String filePath, String key){
+        String data = null;
+        Properties prop = new Properties();
+        File file = new File(filePath);
+
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            prop.load(fis);
+            data = prop.get(key).toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+
+    }
+
+
 }
